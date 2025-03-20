957890c4b06f3176d00ac771be8ffd4587fbad5c
Alexander Brisson <alexanderbrisson@montana.edu>
2025-03-19 18:44:42 -0600

    Add files via upload

---
+++ @WCFServiceOM4006Basic/WCFServiceOM4006Basic.m
@@ -0,0 +1,7 @@
+function obj = WCFServiceOM4006Basic
+
+obj.endpoint = 'http://localhost:9200/Optametra/OM4006/WCFServiceOM4006Basic/';
+obj.wsdl = 'http://localhost:9200/Optametra/OM4006/WCFServiceOM4006Basic/?wsdl';
+
+obj = class(obj,'WCFServiceOM4006Basic');
+
